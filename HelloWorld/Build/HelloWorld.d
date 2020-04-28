7f86f7923d80097cdc672e13ff07d47970494c4e
obeutler <obeutler@online.gibz.ch>
2020-04-28 14:28:31 +0200

    commit

---
+++ HelloWorld/Build/HelloWorld.d
@@ -0,0 +1,51 @@
+D:\Gibz\M242\Avr\HelloWorld\Build\HelloWorld.d \
+ D:\Gibz\M242\Avr\HelloWorld\Build\HelloWorld.o: \
+ D:\Gibz\M242\Avr\HelloWorld\Build\HelloWorld.c \
+ d:\gibz\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\io.h \
+ d:\gibz\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\sfr_defs.h \
+ d:\gibz\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\inttypes.h \
+ d:\gibz\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\lib\gcc\avr\5.4.0\include\stdint.h \
+ d:\gibz\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\stdint.h \
+ d:\gibz\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\iom328p.h \
+ d:\gibz\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\portpins.h \
+ d:\gibz\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\common.h \
+ d:\gibz\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\version.h \
+ d:\gibz\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\fuse.h \
+ d:\gibz\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\lock.h \
+ D:\Gibz\M242\Avr\ToolChain\AvrLib\include/Atmega328P.h \
+ D:\Gibz\M242\Avr\ToolChain\AvrLib\include/avrlib.h \
+ d:\gibz\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\util\delay.h \
+ d:\gibz\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\util\delay_basic.h \
+ d:\gibz\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\math.h
+
+d:\gibz\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\io.h:
+
+d:\gibz\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\sfr_defs.h:
+
+d:\gibz\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\inttypes.h:
+
+d:\gibz\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\lib\gcc\avr\5.4.0\include\stdint.h:
+
+d:\gibz\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\stdint.h:
+
+d:\gibz\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\iom328p.h:
+
+d:\gibz\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\portpins.h:
+
+d:\gibz\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\common.h:
+
+d:\gibz\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\version.h:
+
+d:\gibz\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\fuse.h:
+
+d:\gibz\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\avr\lock.h:
+
+D:\Gibz\M242\Avr\ToolChain\AvrLib\include/Atmega328P.h:
+
+D:\Gibz\M242\Avr\ToolChain\AvrLib\include/avrlib.h:
+
+d:\gibz\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\util\delay.h:
+
+d:\gibz\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\util\delay_basic.h:
+
+d:\gibz\m242\avr\toolchain\avr8-gnu-toolchain-win32_x86\avr\include\math.h:
